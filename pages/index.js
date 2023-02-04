26e31610180be2a157d5679f345124c536121d18
Jason Nutt (He/Him) Developer/Creator <jnutt367@gmail.com>
2023-02-04 17:15:17 -0500

    Update index.js
    https://second-thessalonians.vercel.app/

--- pages/index.js
+++ pages/index.js
@@ -12,10 +12,17 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-         <a href="https://en.wikipedia.org/wiki/1_Timothy_1">I Timothy</a>
-        </h1>
+        
         <div className={styles.hero}>
+             <h2 className={styles.holy}>THE HOLY</h2>
+                <h1 className={styles.bible}>
+                    <a href="https://the-holy-bible.vercel.app/" target="_blank">BIBLE</a>
+                </h1>
+        
+        <h1 className={styles.title}>
+       I TIMOTHY
+        </h1>
+         <div  className={styles.image}>
        <Image 
                                 src="/jesus-as-shepherd_24381-1007.webp"
                                 alt="Jesus teaching"
@@ -23,21 +30,40 @@ export default function Home() {
                                 width={2000}
                                 priority
                               />
-              <div className={styles.nav}>
-    <a href="https://the-holy-bible.vercel.app/">
-            <button className={styles.button}> HOME </button>
+          </div>                        
+   <div className={styles.nav}>           
+      <a href="https://second-thessalonians.vercel.app/">
+             <Image 
+                                src="/702-7024826_computer-back-icon-left-right-arrow-cartoon-arrow.png"
+                                alt="Jesus"
+                                 height={100}
+                                width={100}
+                                priority
+                              />
        </a>
 
          <a href="#PayPalButton">
-            <button className={styles.button}>CONTRIBUTE </button>
+            <Image 
+                                src="/SCROLL-DOWN-button-kopie1.gif"
+                                alt="Jesus"
+                                 height={100}
+                                width={100}
+                                priority
+                              />
+           
         </a> 
-         <a href="https://second-thessalonians.vercel.app/">
-            <button className={styles.button}>PREV </button>
-       </a>           
-        <a href="https://second-timothy.vercel.app/">
-            <button className={styles.button}>NEXT > </button>
-       </a>           
-            </div>
+         <a href="https://second-timothy.vercel.app/">
+           <Image 
+                                src="/jean_victor_balin_arrow_orange_right.png"
+                                alt="Jesus"
+                                 height={100}
+                                width={100}
+                                priority
+                              />
+           
+        </a> 
+         
+               </div>
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h2>Chapter 1 &rarr;</h2>
