d1053ff6d4b6f3d44131da8e53d8ca8ec8b44b83
Jason Nutt (He/Him) Christian Developer/Creator <jnutt367@gmail.com>
2023-07-07 21:38:43 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -71,7 +71,7 @@ export default function Home() {
 
       <div className={styles.iframe}>                     
                 <iframe width="100%" height="315" src="https://www.youtube.com/embed/Gab04dPs_uA" title="YouTube video player" frameborder="2" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> 
-                                
+                                  </div>             
         <div className={styles.grid}>
           <div href="https://nextjs.org/docs" className={styles.card}>
             <h2 className={styles.chapter}> Proverbs I &darr;</h2>
