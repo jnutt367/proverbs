8f4a42874e6c71c29b8b6987177b5b61dc14553f
Jason Nutt (He/Him) Christian Developer/Creator <jnutt367@gmail.com>
2023-12-01 16:58:05 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -24,8 +24,8 @@ export default function Home() {
         </h1>
          <div  className={styles.iframe}>
    <iframe width="100%" height="315" src="https://www.youtube.com/embed/-nGsKeY0Nio?si=jsCd7rgXVDVllKz1&autoplay=1&mute=1" title="YouTube video player" frameborder="5" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-    /*<iframe width="100%" height="315" src="https://www.youtube.com/embed/AzmYV8GNAIM?si=x-GRGggPjePtJ2Pa&autoplay=1&mute=1" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-    */
+    <iframe width="100%" height="315" src="https://www.youtube.com/embed/AzmYV8GNAIM?si=x-GRGggPjePtJ2Pa&autoplay=1&mute=1" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    
     </div>
                            
                                 <p className={styles.parables}>
